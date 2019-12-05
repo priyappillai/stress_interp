b988a6f7de13f8d71a10dd86d4c2e047a1318b0c
priyappillai <pppillai@mit.edu>
2019-12-05 15:03:13 -0500

    Partial catdog image

--- image_gen.py
+++ image_gen.py
@@ -3,7 +3,36 @@ import pandas as pd
 import random
 
 image_folder = "./images/"
-variables = []
+variables = ["fur_pattern", # "plain", "eye_spot", "stripes", "v", "mask", "snout"
+             "fur_lightness",
+             "fur_saturation",
+             "eye_distance", 
+             "eye_height", 
+             "eye_aspect_ratio", 
+             "whiskers", # boolean
+             "whisker_length",
+             "whisker_weight",
+             "face_aspect_ratio",
+             "nose_size",
+             "ear_angle", #up/down
+             "ear_point",
+             "cheek_tufts",
+             "chin_tufts",
+             "head_tufts"]
+
+
+def style(fill_color=(0,0,0), fill_color_type="rgb", stroke_width=0, stroke_color=(0,0,0), stroke_color_type="rgb"):
+    style_str = "fill:%s" %fill_color_type
+    if fill_color_type == "hsl":
+        style_str = style_str+("(%i, %i%%, %i%%)" %fill_color)
+    else:
+        style_str = style_str+("(%i, %i, %i)" %fill_color)
+    style_str = style_str+(";stroke-width:%f;stroke:%s" %(stroke_width, stroke_color_type))
+    if stroke_color_type == "hsl":
+        style_str = style_str+("(%i, %i%%, %i%%)" %stroke_color)
+    else:
+        style_str = style_str+("(%i, %i, %i)" %stroke_color)
+    return style_str
 
 
 def draw_catdog(name, param):
@@ -14,12 +43,29 @@ def draw_catdog(name, param):
     ----------
     name : string
         Filename to save catdog as
-    param : list of floats
-        Variables to set the structure of the catdog face
+    param : dictionary (keys: variables list)
+        Variable values for the structure of the face
     """
     dwg = svgwrite.Drawing(filename=name, debug=True)
+    width = param["face_width"]
+    height = width*param["face_aspect_ratio"]
+    cx = width*2
+    cy = height*2
+    dwg.add(dwg.ellipse(center=(cx,cy),r=(width,height),style=style((45,40,80),"hsl",1)))
+    eye_height = param["eye_height"]
+    eye_width = eye_height*param["eye_aspect_ratio"]
+    eye_distance = param["eye_distance"]
+    dwg.add(dwg.ellipse(center=(cx-eye_distance,cy-height/4),r=(eye_width,eye_height),style=style((0,0,0),"hsl",1)))
+    dwg.add(dwg.ellipse(center=(cx+eye_distance,cy-height/4),r=(eye_width,eye_height),style=style((0,0,0),"hsl",1)))
+    nose_size = param["nose_size"]
+    dwg.add(svgwrite.shapes.Polygon([(cx-nose_size,cy-nose_size/3), (cx+nose_size,cy-nose_size/3), (cx,cy+nose_size)],style=style((0,0,0),"hsl",1)))
+    dwg.add(svgwrite.shapes.Line((cx,cy+nose_size),(cx,cy+nose_size+10),style=style((0,0,0),"hsl",2)))
+    mouth = svgwrite.path.Path(d=("M %i %i" %(cx-15,cy+nose_size+12)))
+    mouth.push_arc((cx,cy+nose_size+10), 45, (10,7), large_arc=False, angle_dir='-', absolute=True)
+    dwg.add(mouth)
     #TODO
     dwg.save()
+
 
 def random_params(n, var_dists):
     """
@@ -30,13 +76,13 @@ def random_params(n, var_dists):
     ----------
     n : int
         Number of parameter settings to generate
-    var_dists : list of distributions
-        A list of probability distributions for each variable
+    var_dists : dictionary of distributions
+        Probability distributions for each variable
     
     Returns
     -------
-    list of lists of floats
-        The parameter settings, variable set in same order as distributions 
+    list of dictionaries (keys: variables list)
+        The parameter settings
     """
     #TODO
     pass
@@ -49,24 +95,25 @@ def all_params(var_settings):
     
     Parameters
     ----------
-    var_settings : list of lists of floats
+    var_settings : dictionary (keys: variables list, values: lists)
         All possible settings for each variable
 
     Returns
     -------
-    list of lists of floats
-        The parameter settings, variable set in same order as settings
+    list of dictionaries (keys: variables list)
+        The parameter settings
     """
     #TODO
     pass
 
 
 if __name__ == "__main__":
-    params = random_params()
-    for i, param in ennumerate(params):
-        name = "catdog_%i" % i
+    params = [{"face_width": 100, "face_aspect_ratio": .75, "eye_height": 6,
+               "eye_aspect_ratio": 1, "eye_distance": 40, "nose_size": 8}]
+    for i, param in enumerate(params):
+        name = "catdog_%i.svg" % i
         draw_catdog(name, param)
-    df = pd.DataFrame(data=params, columns=variables)
+    df = pd.DataFrame(params)
     with pd.ExcelWriter("params.xlsx") as writer:
         df.to_excel(writer)
 
